af991c46fa9f0a5cbed64fa1cbebdf6ab83a3a6f
svda <sander@myxt.nl>
2011-04-14 12:45:07 +0200

    Added Dutch translation.

---
+++ translations/dut-NL/translation.ts
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nl_NL" sourcelanguage="en">
+<context>
+    <name>extension/ngconnect/ngconnect/profile</name>
+    <message>
+        <source>Profile setup</source>
+        <translation>Profiel instellen</translation>
+    </message>
+    <message>
+        <source>One more step is needed to activate your account and to be able to sign in using regular username and password.</source>
+        <translation>Om met je gebruikelijke gebruikersnaam en wachtwoord in te loggen is nog 1 stap nodig.</translation>
+    </message>
+    <message>
+        <source>If you already have a regular account, input your details under &quot;Login to existing account&quot; section.</source>
+        <translation>Als je al een normale account hebt, voer dan de gegevens hiervan in onder &quot;Inloggen met normale account&quot;.</translation>
+    </message>
+    <message>
+        <source>If you do not have a regular account, enter your desired username and password under &quot;Create new account&quot; section.</source>
+        <translation>Als je nog geen normale account hebt, vul dan je gewenste gebruikersnaam en wachtwoord in onder &quot;Nieuwe account aanmaken&quot;.</translation>
+    </message>
+    <message>
+        <source>If you do not wish to create a regular account, simply click the &quot;Skip&quot; button below, and we won&apos;t bother you with this again.</source>
+        <translation>Als je geen normale account wilt aanmaken, klik dan op &quot;Overlaan&quot; en we zullen je hier niet meer mee lastig vallen.</translation>
+    </message>
+    <message>
+        <source>Account with the email address from your social network (%1) already exists. Please enter your login details below to connect to that account.</source>
+        <translation>Er bestaat al een account met het e-mailadres dat je op %1 gebruikt. Vul de inloggegevens hieronder in om deze account te koppelen.</translation>
+    </message>
+    <message>
+        <source>If you forgot your password, request a new one</source>
+        <translation>Als je je wachtwoord vergeten bent, vraag dan een nieuwe aan</translation>
+    </message>
+    <message>
+        <source>here</source>
+        <translation>hier</translation>
+    </message>
+    <message>
+        <source>A valid username and password is required to login.</source>
+        <translation>Vul een geldige gebruikersnaam en wachtwoord in om in te loggen.</translation>
+    </message>
+    <message>
+        <source>You are not allowed to access the site.</source>
+        <translation>Je hebt geen toegang tot de site.</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation>Gebruikersnaam</translation>
+    </message>
+    <message>
+        <source>E-mail</source>
+        <translation>E-mailadres</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Wachtwoord</translation>
+    </message>
+    <message>
+        <source>Repeat password</source>
+        <translation>Wachtwoord herhalen</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Opslaan</translation>
+    </message>
+    <message>
+        <source>Skip</source>
+        <translation>Overslaan</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Inloggen</translation>
+    </message>
+    <message>
+        <source>Welcome</source>
+        <translation>Welkom</translation>
+    </message>
+    <message>
+        <source>Login to existing account</source>
+        <translation>Inloggen met bestaande account</translation>
+    </message>
+    <message>
+        <source>Create new account</source>
+        <translation>Nieuwe account aanmaken</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ngconnect/ngconnect/success</name>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+</TS>
