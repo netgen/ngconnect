8fe604b5a7803b848435c3ced207f5988af80102
Sebastian Blank <sebastian.bl@gmx.de>
2013-03-20 08:14:35 +0100

    Added German translation

---
+++ translations/ger-DE/translation.ts
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE" sourcelanguage="en">
+<context>
+    <name>extension/ngconnect/ngconnect/profile</name>
+    <message>
+        <source>Profile setup</source>
+        <translation>Profil erstellen</translation>
+    </message>
+    <message>
+        <source>One more step is needed to activate your account and to be able to sign in using regular username and password.</source>
+        <translation>Ein weiterer Schritt ist erforderlich, um Ihr Konto zu aktivieren.</translation>
+    </message>
+    <message>
+        <source>If you already have a regular account, input your details under &quot;Login to existing account&quot; section.</source>
+        <translation>Wenn Sie bereits ein Konto haben, geben Sie Ihre Daten unter &quot;Anmelden bei bestehenden Konto&quot; ein.</translation>
+    </message>
+    <message>
+        <source>If you do not have a regular account, enter your desired username and password under &quot;Create new account&quot; section.</source>
+        <translation>Wenn Sie noch kein Konto haben, geben Sie Ihren gewünschten Benutzernamen und Passwort unter &quot;Neues Konto&quot; ein.</translation>
+    </message>
+    <message>
+        <source>If you do not wish to create a regular account, simply click the &quot;Skip&quot; button below, and we won&apos;t bother you with this again.</source>
+        <translation>Wenn Sie kein Konto erstellen möchten, klicken Sie einfach auf den &quot;Überspringen&quot;-Knopf.</translation>
+    </message>
+    <message>
+        <source>Account with the email address from your social network (%1) already exists. Please enter your login details below to connect to that account.</source>
+        <translation>Ein Konto mit der E-Mail-Adresse (%1) ist bereits vorhanden. Bitte geben Sie Ihre Login-Daten ein, um sich mit diesem Konto zu verbinden.</translation>
+    </message>
+    <message>
+        <source>If you forgot your password, request a new one</source>
+        <translation>Wenn Sie Ihr Passwort vergessen haben, können Sie hier ein neues anfordern</translation>
+    </message>
+    <message>
+        <source>here</source>
+        <translation>hier</translation>
+    </message>
+    <message>
+        <source>A valid username and password is required to login.</source>
+        <translation>Bitte geben Sie einen gültigen Benutzernamen und Passwort ein.</translation>
+    </message>
+    <message>
+        <source>You are not allowed to access the site.</source>
+        <translation>Sie haben keinen Zugriff auf diese Seite.</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation>Benutzernamen</translation>
+    </message>
+    <message>
+        <source>E-mail</source>
+        <translation>E-Mail</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Passwort</translation>
+    </message>
+    <message>
+        <source>Repeat password</source>
+        <translation>Passwort erneut eingeben</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Speichern</translation>
+    </message>
+    <message>
+        <source>Skip</source>
+        <translation>Überspringen</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Login</translation>
+    </message>
+    <message>
+        <source>Welcome</source>
+        <translation>Willkommen</translation>
+    </message>
+    <message>
+        <source>Login to existing account</source>
+        <translation>Anmelden bei bestehenden Konto</translation>
+    </message>
+    <message>
+        <source>Create new account</source>
+        <translation>Neues Konto</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ngconnect/ngconnect/success</name>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+</TS>
